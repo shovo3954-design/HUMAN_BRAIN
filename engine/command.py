3d8d087ec58f874c28a60af57a5bcb2431d87735
shuvo3954-design <shovo3954@gamil.com>
2026-03-28 17:22:20 +0600

    adding new file

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,6 @@
 import pyttsx3
+import speech_recognition as sr
+
 
 def speak(text):
     engine = pyttsx3.init("sapi5")
@@ -9,4 +11,25 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     
-speak("Hi, I am robot who acts like human.")
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+        
+        
+    try:
+        print("Recognizing")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said {query}")
+        
+    except Exception as e:
+        return ""
+    return query.lower()
+
+text = takecommand()
+    
+speak(text)
